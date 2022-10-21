4f3294988b1385139b29c56c92e122b16ead89ee
Hiltonmp <110299868+Hiltonmp@users.noreply.github.com>
2022-10-21 15:46:36 -0300

    eba
    eba

---
+++ epa.js
@@ -0,0 +1,8 @@
+function letra(){
+    var l1 = parseFloat(document.getElementById('c2').value)
+
+    if(l1 = 'A'||'E'||'I'||'O'||'U'){
+    alert("É uma vogal")
+    }else
+    alert("É uma consoante")
+}
